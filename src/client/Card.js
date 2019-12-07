667daa21010f748a30b70054bcb43e9c913ec015
Daniel Beach <dtmhawk@gmail.com>
2019-12-08 00:28:28 +1000

    card smallernized and changed to em spacing

--- src/client/Card.js
+++ src/client/Card.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import logo from './arrow.png';
+import heart from './heart.png';
 class Card extends Component {
     constructor() {
         return super();
     }
     render() {
+        const explicitObject = "";
+        let implicitObject = "";
+        let count = 2;
+        for(let i = 0; i < count; i++){
+            implicitObject = implicitObject + "<img src={logo}/>"
+        }
         return (
             <div>
                 <div className="cardConatiner">
@@ -12,7 +19,7 @@ class Card extends Component {
                         <div className="cardHeaderContainer">
                             <div className="cardHeaderLeftImage"> <img src={logo} className="logoLeft" /></div>
                             <div className="cardtitleHolder">
-                                {/* <h1 className="cardTitle">Full Plate</h1> */}
+                                <h1 className="cardTitle">Full Plate</h1>
                             </div>
                             <div className="cardHeaderRightImage">  <img src={logo} className="logoRight" /> </div>
                         </div>
@@ -27,14 +34,18 @@ class Card extends Component {
                                 </div>
                             </div>
                             <div className="cardCentreBody">
-                                <div className="Implicit Stats"></div>
-                                <div className="explicitStats"> </div>
-                                <div className="mainImage"> </div>
+                                <div className="cardImplicitStats">{implicitObject}</div>
+                                <div className="cardExplicitStats"> {explicitObject}</div>
+                                <div className="cardMainImage">
+                                    <div className="cardShine">
+                                        <img src="https://gamepedia.cursecdn.com/pathofexile_gamepedia/c/c2/Sun_Plate_inventory_icon.png?version=5fc9af4d7887bca14819acd9384a462e"></img>
+                                    </div>
+                                </div>
                             </div>
                             <div className="cardRightBody">
                                 <div className="durabilitySection">
                                     <div className="durabilityTitle">
-                                        {/* <h2>Durability</h2> */}
+                                        {/* <h2>durability</h2> */}
                                     </div>
                                     <div className="lightType"><img /></div>
                                     <div className="lightType"><img /></div>
